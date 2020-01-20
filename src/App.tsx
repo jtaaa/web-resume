81c10b0653588a15f8f05f0fc5232873f271a315
Joshua Allum <alome@Joshuas-MBP.lan>
2020-01-19 22:21:37 -0500

    Add component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from 'components/Header';
+import Body from 'components/Body';
+import Footer from 'components/Footer';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="A5">
+      <section className="sheet">
+        <Header />
+        <Body />
+        <Footer />
+      </section>
+    </body>
   );
-}
+};
 
 export default App;
