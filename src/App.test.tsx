6c2214a0cbaa6b0f73057abf53609ef1c6c20e85
Joshua Allum <joshuatallum@gmail.com>
2020-01-21 02:27:52 -0500

    Update App unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders my name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Joshua Allum/i);
   expect(linkElement).toBeInTheDocument();
 });
