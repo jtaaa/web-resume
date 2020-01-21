fa68fd3125e8c9fdc1fd321537a6e9ee9caca0c2
Joshua Allum <joshuatallum@gmail.com>
2020-01-21 04:25:50 -0500

    Add Cognite copy

--- src/components/Body/Experience/Cognite/Cognite.tsx
+++ src/components/Body/Experience/Cognite/Cognite.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Box from 'components/Box';
+import Body from 'components/text/Body';
 import Heading from '../Heading';
 import CogniteLogo from './cognite-logo.jpeg';
+import ExternalLink from 'components/ExternalLink';
+import Highlight from 'components/text/Highlight';
 
 const Cognite = () => {
   return (
@@ -13,6 +16,31 @@ const Cognite = () => {
         logoSrc={CogniteLogo}
         logoAlt="Cognite AS logo"
       />
+      <Body>
+        Worked daily on front-end of production application{' '}
+        <ExternalLink href="https://insight.cogniteapp.com/">
+          Asset Data Insight
+        </ExternalLink>{' '}
+        with monthly sprints and rollouts.
+      </Body>
+      <Body>
+        Implemented <Highlight>design curve overlays on scatterplots</Highlight>{' '}
+        with option to upload CSV or manually enter X-Y pairs, solving
+        company-wide{' '}
+        <ExternalLink href="https://www.enggcyclopedia.com/2011/09/pump-performance-curves/">
+          pump performance curve
+        </ExternalLink>{' '}
+        use case.
+      </Body>
+      <Body>
+        Significantly improved <Highlight>development experience</Highlight>,{' '}
+        <Highlight>linting</Highlight> and{' '}
+        <Highlight>testing utilities</Highlight> of custom graphing library{' '}
+        <ExternalLink href="https://github.com/cognitedata/griff-react">
+          Griff
+        </ExternalLink>
+        .
+      </Body>
     </Box>
   );
 };
