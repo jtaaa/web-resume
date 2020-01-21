b9f1b2936e2f0335c6c8b2bff959e4817d4fc33e
Joshua Allum <joshuatallum@gmail.com>
2020-01-21 06:59:06 -0500

    Add and use logos for contact info

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,31 +3,51 @@ import Box from 'components/Box';
 import ExternalLink from 'components/ExternalLink';
 import Canon from 'components/text/Canon';
 import Body from 'components/text/Body';
-import { HeaderContainer } from './elements';
+import { HeaderContainer, LinksGrid } from './elements';
+
+import Globe from './Globe.svg';
+import Email from './Email.svg';
+import Phone from './Phone.svg';
+import Github from './Github.svg';
+import LinkedIn from './LinkedIn.svg';
 
 const Header = () => {
   return (
     <HeaderContainer display="flex" alignItems="center">
       <Canon>Joshua Allum</Canon>
-      <Box flex={1}>
-        <Body display="flex" justifyContent="center">
-          <ExternalLink href="https://jallum.xyz/">jallum.xyz</ExternalLink>
-          <ExternalLink ml={3} href="mailto:joshuatallum@gmail.com">
-            joshuatallum
-          </ExternalLink>
-          <ExternalLink ml={3} href="tel:+16477814107">
-            647-781-4107
-          </ExternalLink>
-        </Body>
-        <Body display="flex" justifyContent="center">
-          <ExternalLink ml={3} href="https://github.com/jtaaa">
-            jtaaa
-          </ExternalLink>
-          <ExternalLink ml={3} href="https://linkedin.com/in/joshua-allum/">
-            joshua-allum
-          </ExternalLink>
-        </Body>
-      </Box>
+      <LinksGrid flex={1}>
+        <ExternalLink href="https://jallum.xyz/">
+          <Box display="flex" alignItems="center">
+            <img height={11} src={Globe} alt="Globe" />
+            <Body ml={1}>jallum.xyz</Body>
+          </Box>
+        </ExternalLink>
+        <ExternalLink ml={3} href="mailto:joshuatallum@gmail.com">
+          <Box display="flex" alignItems="center">
+            <img height={11} src={Email} alt="Email" />
+            <Body ml={1}>joshuatallum</Body>
+          </Box>
+        </ExternalLink>
+        <ExternalLink ml={3} href="tel:+16477814107">
+          <Box display="flex" alignItems="center">
+            <img height={11} src={Phone} alt="Phone" />
+            <Body ml={1}>647-781-4107</Body>
+          </Box>
+        </ExternalLink>
+        <div />
+        <ExternalLink ml={3} href="https://github.com/jtaaa">
+          <Box display="flex" alignItems="center">
+            <img height={11} src={Github} alt="Github" />
+            <Body ml={1}>jtaaa</Body>
+          </Box>
+        </ExternalLink>
+        <ExternalLink ml={3} href="https://linkedin.com/in/joshua-allum/">
+          <Box display="flex" alignItems="center">
+            <img height={11} src={LinkedIn} alt="LinkedIn" />
+            <Body ml={1}>joshua-allum</Body>
+          </Box>
+        </ExternalLink>
+      </LinksGrid>
     </HeaderContainer>
   );
 };
--- src/components/Header/elements.ts
+++ src/components/Header/elements.ts
@@ -2,3 +2,15 @@ import styled from 'styled-components/macro';
 import Box from 'components/Box';
 
 export const HeaderContainer = styled(Box)``;
+HeaderContainer.defaultProps = {
+  pb: 2,
+};
+
+export const LinksGrid = styled(Box)`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: auto auto;
+`;
+LinksGrid.defaultProps = {
+  ml: 4,
+};
