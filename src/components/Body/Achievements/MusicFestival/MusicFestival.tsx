7ffba3ac0b01c6e01691a8668d0750a3e4099571
Joshua Allum <joshuatallum@gmail.com>
2020-09-16 01:04:34 -0400

    Add music festival link

--- src/components/Body/Achievements/MusicFestival/MusicFestival.tsx
+++ src/components/Body/Achievements/MusicFestival/MusicFestival.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Box from 'components/Box';
 import Heading from '../Heading';
+import ExternalLink from 'components/ExternalLink';
 
 const MusicFestival = () => {
   return (
     <Box>
-      <Heading title="Music Festival Band Winner" year="2012-16" />
+      <Heading
+        title={
+          <>
+            <ExternalLink href="http://ttmusicfestival.org">
+              Music Festival
+            </ExternalLink>{' '}
+            Band Winner
+          </>
+        }
+        year="2012-16"
+      />
     </Box>
   );
 };
